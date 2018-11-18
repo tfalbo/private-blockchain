147bb49884582a91e1889be927f98a4011fcfd6d
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-11-17 23:40:29 -0200

    turns one function into promise

--- levelSandbox.js
+++ levelSandbox.js
@@ -23,15 +23,17 @@ function getLevelDBData(key){
 
 // Add data to levelDB with value
 function addDataToLevelDB(value) {
+  return new Promise (function(resolve, reject) {
     let i = 0;
     db.createReadStream().on('data', function(data) {
           i++;
         }).on('error', function(err) {
-            return console.log('Unable to read data stream!', err)
+            console.log('Unable to read data stream!', err)
         }).on('close', function() {
           console.log('Block #' + i);
           addLevelDBData(i, value);
         });
+  });   
 }
 
 /* ===== Testing ==============================================================|
